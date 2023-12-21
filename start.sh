a07b35b68137c05b55d84dd3ce4f4de1dccfab33
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-21 14:07:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/UnknownBotz/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/dhanush2bot/dhanushpro.git /dhanushpro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EasyAdvAutoFilter
+  git clone $UPSTREAM_REPO /dhanushpro
 fi
-cd /EasyAdvAutoFilter
+cd /dhanushpro
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
